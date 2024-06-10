37cfcaf32fb2e23d831927884cf1acbe26a98834
Jung-Hun Shin <shinjunghun77@gmail.com>
2024-06-10 13:08:57 +0900

    Delete FrameScriptableObject.cs

--- FrameScriptableObject.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Frame", menuName = "ScriptableObjects/FrameScriptableObject", order = 1)]
-public class FrameScriptableObject : ScriptableObject
-{
-
-}
